d9d03ea7f5ebeb92ccce6c94055b72cfe8b8a52a
Saulul <35707165+Saulul@users.noreply.github.com>
2024-03-24 19:35:16 +0000

    yes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+//import autoprefixer from 'autoprefixer';
+//autoprefixer fails the build, I have no idea what autoprefixer is and where it came from :D
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
            tailwindcss(),
-           autoprefixer(),
+//           autoprefixer(),
            ],
   server: {
     host: '0.0.0.0',
