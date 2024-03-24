c1f86ab3589082879e24ca44820550cb515ed073
Saulul <35707165+Saulul@users.noreply.github.com>
2024-03-24 19:29:50 +0000

    find the culprit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-//import tailwindcss from 'tailwindcss';
+import tailwindcss from 'tailwindcss';
 //import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
-//           tailwindcss(),
+           tailwindcss(),
 //           autoprefixer(),
            ],
   server: {
