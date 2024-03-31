2d6cd7901b66c755c61a8f71f71d68424ee69fab
Elysia Todd <87079220+Elysiatodd@users.noreply.github.com>
2024-03-31 20:49:57 +0000

    Added Purchase ticket for event

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import "./index.css";
+import './Purchase.css'; // Import CSS file for styling
+
 import { FC } from "react";
 import OpenAI from "openai";
 import "./App.css";
@@ -9,6 +11,11 @@ import Notifications from "./components/Notifications";
 import Chat from "./components/Chatbot/Chat"; // Import the Chat component
 import UserRegister from "./components/UserRegister";
 import UserLogin from "./components/UserLogin";
+import NoPage from "./components/NoPage.jsx";
+
+import Stripeint from "./components/stripeint.jsx";
+import Buytickets from "./components/BuyTickets.jsx";
+import Purchase from "./components/Purchase.jsx";
 
 const App: FC = () => {
   return (
@@ -17,9 +24,14 @@ const App: FC = () => {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/map" element={<Map />} />
+        <Route path="/purchase/:eventId" element={<Purchase/>} />
+
         <Route path="/notifications" element={<Notifications />} />
         <Route path="/register" element={<UserRegister />} />
         <Route path="/login" element={<UserLogin />} />
+        <Route path="*" element={<NoPage />} />
+        <Route path="/stripe-payment" element={<Stripeint />} />
+        <Route path="/buytickets" element={<Buytickets />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/BuyTickets.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Event Management App</h1>
+      <PaymentForm />
+    </div>
+  );
+};
+
+export default App;
