b252e56a05d11d112307043318ec6170d4e173b7
Saulul <35707165+Saulul@users.noreply.github.com>
2024-04-01 07:24:12 +0330

    idk its 7 am

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import "./index.css";
-import './Purchase.css'; // Import CSS file for styling
-import { FC } from "react";
-import OpenAI from "openai";
+import './Purchase.css';
+import {FC} from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Map from "./components/Map";
 import Homepage from "./components/Homepage";
-import Chat from "./components/Chatbot/Chat"; // Import the Chat component
+import Chat from "./components/Chatbot/Chat";
 import UserRegister from "./components/UserRegister";
 import UserLogin from "./components/UserLogin";
-import { UserProvider } from "./components/UserContext";
+import {UserProvider} from "./components/UserContext";
 import ProfilePage from "./components/ProfilePage";
 import MyEvents from "./components/MyEvents";
 import NoPage from "./components/NoPage.jsx";
@@ -20,29 +19,27 @@ import Purchase from "./components/Purchase.jsx";
 import AnalyticsDashboard from "./components/AnalyticsDashboard";
 
 const App: FC = () => {
-  return (
-    <UserProvider>
-      <div className="flex flex-col items-center pt-8 bg-gray-50 rounded-3xl">
-        <BrowserRouter>
-          <Chat />
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/map" element={<Map />} />
-              <Route path="/purchase/:eventId" element={<Purchase/>} />
-            <Route path="/register" element={<UserRegister />} />
-            <Route path="/login" element={<UserLogin />} />
-              <Route path="*" element={<NoPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/myevents" element={<MyEvents />} />
-              <Route path="/stripe-payment" element={<Stripeint />} />
-              <Route path="/buytickets" element={<Buytickets />} />
-              <Route path="/analytics" element={<AnalyticsDashboard />} />
-              <Route path="/events/:id" element={<Event />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </UserProvider>
-  );
+    return (
+        <UserProvider>
+            <BrowserRouter>
+                <Chat/>
+                <Routes>
+                    <Route path="/" element={<Homepage/>}/>
+                    <Route path="/map" element={<Map/>}/>
+                    <Route path="/purchase/:eventId" element={<Purchase/>}/>
+                    <Route path="/register" element={<UserRegister/>}/>
+                    <Route path="/login" element={<UserLogin/>}/>
+                    <Route path="*" element={<NoPage/>}/>
+                    <Route path="/profile" element={<ProfilePage/>}/>
+                    <Route path="/myevents" element={<MyEvents/>}/>
+                    <Route path="/stripe-payment" element={<Stripeint/>}/>
+                    <Route path="/buytickets" element={<Buytickets/>}/>
+                    <Route path="/analytics" element={<AnalyticsDashboard/>}/>
+                    <Route path="/events/:id" element={<Event/>}/>
+                </Routes>
+            </BrowserRouter>
+        </UserProvider>
+    );
 };
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
     };
 
     return (
-        <div className="flex gap-5 justify-between mb-9 px-5 w-full max-w-[1200px] max-md:flex-wrap max-md:max-w-full">
+        <div className="flex gap-5 justify-between mb-9 px-5 w-full max-w-[1200px] max-md:flex-wrap max-md:max-w-full pt-8">
             <div className="flex-auto text-4xl font-bold text-zinc-400">
                 <Link to="/">
                     Event<span className="text-zinc-500">Hive</span>
